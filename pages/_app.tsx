056cbd3a64c9b9b274a0c8727d2d7beaaa369fde
Krushil Naik <krushilnaik96@gmail.com>
2022-07-17 14:24:29 -0400

    Fix horizontal scrollbar showing

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,7 @@ import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div className="w-screen min-h-screen bg-slate-900 text-slate-200 p-3">
+    <div className="w-screen min-h-screen max-w-full bg-slate-900 text-slate-200 p-3">
       <nav className="flex justify-between items-center">
         <div className="bg-rose-600 w-12 h-12 rounded-full grid place-content-center text-2xl font-sans font-semibold">
           K
