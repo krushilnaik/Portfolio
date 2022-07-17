2ba0be0c7324ed6b3f2cd2f47c0508bd194fff04
Krushil Naik <krushilnaik96@gmail.com>
2022-07-17 14:08:35 -0400

    Skeleton main layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,27 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className="w-screen min-h-screen bg-slate-900 text-slate-200 p-3">
+      <nav className="flex justify-between items-center">
+        <div className="bg-rose-600 w-12 h-12 rounded-full grid place-content-center text-2xl font-sans font-semibold">
+          K
+        </div>
+        <ul className="flex gap-2">
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </nav>
+      <main>
+        <Component {...pageProps} />
+      </main>
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
