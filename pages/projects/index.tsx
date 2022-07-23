b5c8e56d083cf4d615ac241689b01e4931315459
Krushil Naik <krushilnaik96@gmail.com>
2022-07-22 20:04:22 -0400

    Styling tweaks

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -7,7 +7,7 @@ function Projects() {
     <ul className="flex gap-3 flex-wrap justify-center">
       {projects.map((blob, i) => (
         <li
-          className="min-w-[360px] bg-slate-700 hover:bg-slate-600 transition-colors cursor-pointer"
+          className="min-w-[360px] bg-slate-700 hover:bg-slate-600 rounded-md transition-colors cursor-pointer"
           key={`project-${i}`}
         >
           <ProjectCard projectName={blob} />
