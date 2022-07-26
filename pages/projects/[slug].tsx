c910aa1c634240d14e98e9a7403bad8e08478fa6
Krushil Naik <krushilnaik96@gmail.com>
2022-07-26 10:38:30 -0400

    Add some animations

--- pages/projects/[slug].tsx
+++ pages/projects/[slug].tsx
@@ -8,7 +8,12 @@ import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
 interface Project {
   title: string;
   accentColor: string;
-  techStackCollection: object;
+  techStackCollection: {
+    items: {
+      label: string;
+      accentColor: string;
+    }[];
+  };
   description: {
     json: any;
   };
@@ -20,53 +25,92 @@ interface Props {
 const ProjectPage = ({ project }: Props) => {
   const router = useRouter();
 
-  // const { title, accentColor, description } = project;
-  // const title = "test";
-  // const accentColor = "#333";
-
-  const mobileVariants: Variants = {
-    open: {
-      width: "100vw",
-      height: "100vw",
+  const previewVariants: Variants = {
+    visible: {
+      y: 0,
+      opacity: 1,
+      transition: {
+        delay: 0.4,
+        duration: 0.6,
+      },
+    },
+    hidden: {
+      y: -50,
+      opacity: 0,
       transition: {
         duration: 0.6,
       },
     },
-    close: {
-      width: 0,
-      height: 0,
+  };
+
+  const mobileVariants: Variants = {
+    visible: {
+      width: "110vw",
+      height: "110vw",
       transition: {
+        duration: 0.6,
+      },
+    },
+    hidden: {
+      width: "10vw",
+      height: "10vw",
+      transition: {
+        delay: 0.3,
         duration: 0.5,
       },
     },
   };
 
   const desktopVariants: Variants = {
-    open: {
-      width: "70vw",
+    visible: {
+      width: "65vw",
       transition: {
         duration: 0.6,
       },
     },
-    close: {
+    hidden: {
       width: 0,
       transition: {
+        delay: 0.3,
         duration: 0.5,
       },
     },
   };
 
+  const containerVariants: Variants = {
+    visible: {
+      transition: {
+        staggerChildren: 0.2,
+        staggerDirection: -1,
+      },
+    },
+  };
+
+  const techStackVariants: Variants = {
+    visible: {
+      x: 0,
+      opacity: 1,
+      transition: {
+        duration: 0.2,
+      },
+    },
+    hidden: {
+      x: -50,
+      opacity: 0,
+    },
+  };
+
   const DesktopBackground = () => (
-    <div className="hidden md:block absolute top-0 left-0 max-w-[100%] overflow-hidden">
+    <div
+      className="hidden md:block absolute top-0 left-0 max-w-[100%] overflow-hidden"
+      key="project_desktop_wrapper"
+    >
       <motion.div
         variants={desktopVariants}
         style={{
           clipPath: "polygon(0 0, 80% 0%, 55% 100%, 0% 100%)",
           backgroundColor: project.accentColor,
         }}
-        initial="close"
-        animate="open"
-        exit="close"
         key="project_desktop_background"
         className="bg-red-900 h-screen"
       ></motion.div>
@@ -74,12 +118,12 @@ const ProjectPage = ({ project }: Props) => {
   );
 
   const MobileBackground = () => (
-    <div className="absolute md:hidden top-0 left-0 w-screen h-screen max-w-[100%] overflow-hidden z-10">
+    <div
+      className="absolute md:hidden top-0 left-0 w-screen h-screen max-w-[100%] overflow-hidden z-10"
+      key="project_mobile_wrapper"
+    >
       <motion.div
         variants={mobileVariants}
-        initial="close"
-        animate="open"
-        exit="close"
         key="project_mobile_background"
         className="absolute -top-[40vw] left-1/2 -translate-x-1/2 z-10 rounded-full bg-red-900"
         style={{ backgroundColor: project.accentColor }}
@@ -90,17 +134,19 @@ const ProjectPage = ({ project }: Props) => {
   return router.isFallback ? (
     <h1>loading...</h1>
   ) : (
-    <>
+    <motion.div initial="hidden" animate="visible" exit="hidden">
       {/* Render the background based on screen size */}
       <DesktopBackground />
       <MobileBackground />
 
       <div className="relative z-20 md:p-9 flex flex-wrap justify-center gap-y-9 gap-x-48">
         <figure className="flex flex-col items-center gap-6">
-          <img
+          <motion.img
             src=""
+            variants={previewVariants}
             alt={`${project.title} screenshot`}
-            className="bg-rose-600 rounded-md w-44 h-80 lg:w-[550px] lg:h-[300px]"
+            key="project_demo"
+            className="bg-rose-600 rounded-lg w-44 h-80 lg:w-[550px] lg:h-[300px] drop-shadow-2xl"
           />
           <figcaption className="text-center text-3xl md:text-4xl">
             {project.title}
@@ -108,16 +154,21 @@ const ProjectPage = ({ project }: Props) => {
         </figure>
 
         <div className="grid gap-9">
-          <ul className="flex flex-wrap gap-2 my-2 mx-auto justify-center md:justify-start h-fit w-80">
-            {[0, 1, 2, 3, 4, 5].map((i) => (
-              <li
+          <motion.ul
+            variants={containerVariants}
+            className="flex flex-wrap gap-2 my-2 mx-auto justify-center md:justify-start h-fit w-80"
+          >
+            {project.techStackCollection.items.map(({ label, accentColor }, i) => (
+              <motion.li
+                variants={techStackVariants}
                 key={`tech-${i}`}
-                className="bg-rose-300 border-rose-400 border-2 text-slate-600 font-bold rounded-full px-2 w-fit h-8"
+                className="border-2 font-bold rounded-full px-2 w-fit h-8"
+                style={{ borderColor: accentColor, backgroundColor: `${accentColor}77` }}
               >
-                {`Tech ${i + 1}`}
-              </li>
+                {label}
+              </motion.li>
             ))}
-          </ul>
+          </motion.ul>
           <article>{documentToReactComponents(project.description.json)}</article>
         </div>
       </div>
@@ -137,7 +188,7 @@ const ProjectPage = ({ project }: Props) => {
           </a>
         </Link>
       </div>
-    </>
+    </motion.div>
   );
 };
 
