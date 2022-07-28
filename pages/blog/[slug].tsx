0aaa4addd3e03a4991884934b14c23ed5373a8d0
Krushil Naik <krushilnaik96@gmail.com>
2022-07-28 14:49:29 -0400

    Plan for blog

---
+++ pages/blog/[slug].tsx
@@ -0,0 +1,5 @@
+function BlogPost() {
+  return <div>Enter</div>;
+}
+
+export default BlogPost;
---
+++ pages/blog/index.tsx
@@ -0,0 +1,5 @@
+function BlogHome() {
+  return <div className="text-center text-4xl font-mono">Blog</div>;
+}
+
+export default BlogHome;
