bba85271ec3c2930c9abb83941a83145f9ba1733
Krushil Naik <krushilnaik96@gmail.com>
2022-07-29 08:52:44 -0400

    Add loader GIF

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -1,9 +1,10 @@
+import Image from "next/image";
 import React from "react";
 
 function Loader() {
   return (
     <div className="w-screen h-screen absolute grid place-content-center z-50 top-0 left-0 bg-black/50 text-3xl">
-      Loading...
+      <Image src="/images/loader.gif" width={150} height={150} alt="loading" />
     </div>
   );
 }
