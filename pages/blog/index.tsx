6fc3e1a2f299c000759a5ee3f79f8e9ddcd9258a
Krushil Naik <krushilnaik96@gmail.com>
2022-07-28 22:05:22 -0400

    Label blog as coming soon

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,5 +1,5 @@
 function BlogHome() {
-  return <div className="text-center text-4xl font-mono">Blog</div>;
+  return <div className="text-center text-4xl font-mono">Coming soon</div>;
 }
 
 export default BlogHome;
