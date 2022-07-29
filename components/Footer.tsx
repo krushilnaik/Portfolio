babaa6ea4a28e4b1aea68b5cb375a4f48324c30e
Krushil Naik <krushilnaik96@gmail.com>
2022-07-28 22:11:41 -0400

    Styling tweaks

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -8,7 +8,7 @@ function Footer(props: Props) {
   const {} = props;
 
   return (
-    <footer className="flex flex-col p-4 z-40 items-center gap-3 h-64 bg-black/5 dark:bg-white/5 mt-6">
+    <footer className="relative flex flex-col p-4 z-40 items-center gap-3 h-64 bg-black/5 dark:bg-white/5 mt-6">
       <h2 className="text-2xl">Social Links</h2>
       <div className="flex gap-3">
         <span className="bg-slate-50/5 rounded-full w-12 h-12 grid place-content-center select-none hover:bg-slate-50/10 hover:text-rose-400 transition-colors cursor-pointer">
@@ -20,6 +20,9 @@ function Footer(props: Props) {
           </a>
         </Link>
       </div>
+      <p className="text-lg absolute bottom-7 text-slate-200">
+        Accepts bribes in the form of Taco Bell 🌮
+      </p>
     </footer>
   );
 }
