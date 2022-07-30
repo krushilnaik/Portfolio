4ad69009482ac650a7278b4d62efd48c6153ed0f
Krushil Naik <krushilnaik96@gmail.com>
2022-07-30 00:57:06 -0400

    Fix loader glitch
    - you can still scroll when it's in view, don't know if I'll fix that

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 function Loader() {
   return (
-    <div className="w-screen h-screen absolute grid place-content-center z-50 top-0 left-0 bg-black/50 text-3xl">
+    <div className="w-screen h-screen sticky grid place-content-center z-50 top-0 bottom-0 left-0 bg-black/50 text-3xl">
       <Image src="/images/loader.gif" width={150} height={150} alt="loading" />
     </div>
   );
